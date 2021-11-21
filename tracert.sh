dc2e25bb07710835a3f2295798e550dbeb06871f
hmx27-daisuki <70591873+hmx27-daisuki@users.noreply.github.com>
2021-11-22 01:53:35 +0800

    Update tracert.sh

--- tracert.sh
+++ tracert.sh
@@ -7,3 +7,6 @@ unzip besttrace4linux.zip -d besttrace && rm besttrace4linux.zip
 
 #更改besttrace权限
 chmod -R +x /root/besttrace/besttrace
+
+#删除tracert.sh（目的是要安装besttrace,安装之后就不需要再次执行）
+rm tracert.sh
